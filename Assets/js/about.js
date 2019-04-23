5c9d970d1d3412228f6b4361bde9543a4ab2b0e9
Samantha <simpsonnsamantha@yahoo.com>
2019-04-23 15:35:12 -0700

    web updates

--- Assets/js/about.js
+++ Assets/js/about.js
@@ -1,5 +1,6 @@
-/* Demo purposes only */
-$(".hover").mouseleave(
+//element in html. 
+// ".hover" is part of html and afterwards is the action. 
+$(".hover").mouseleave( 
   function () {
     $(this).removeClass("hover");
   }
